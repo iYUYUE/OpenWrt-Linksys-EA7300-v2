d01b0d47d4d2def08cdbb5da5424dde0d4c82038
iYUYUE <admin@iyuyue.com>
2023-09-02 11:20:05 -0700

    Add pbr

--- diy-part2.sh
+++ diy-part2.sh
@@ -25,6 +25,11 @@ git pull origin master
 git branch --set-upstream-to=origin/master master
 cd ..
 
+# Clone source.openwrt.melmac.net.git
+git clone -b master https://github.com/stangri/source.openwrt.melmac.net source.openwrt.melmac.net
+cp -r source.openwrt.melmac.net/pbr package/pbr
+cp -r source.openwrt.melmac.net/luci-app-pbr package/luci/applications/luci-app-pbr
+
 # 编译 po2lmo
 pushd luci-app-openclash/tools/po2lmo
 make && sudo make install
