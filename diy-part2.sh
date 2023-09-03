60717ecdb1d755809a6fd0a817979d73c2024e99
iyuyue <iyuyue@iyuyue.net>
2023-09-03 17:48:56 +0000

    Fix luci-app-pbr include path

--- diy-part2.sh
+++ diy-part2.sh
@@ -39,6 +39,6 @@ cd ..
 git clone -b master https://github.com/stangri/source.openwrt.melmac.net source.openwrt.melmac.net
 cp -r source.openwrt.melmac.net/dnsmasq package/dnsmasq
 cp -r source.openwrt.melmac.net/pbr package/pbr
-mkdir -p package/luci/applications
-cp -r source.openwrt.melmac.net/luci-app-pbr package/luci/applications/luci-app-pbr
+cp -r source.openwrt.melmac.net/luci-app-pbr package/luci-app-pbr
+sed -i "s|../../luci.mk|../../feeds/luci/luci.mk|g" package/luci-app-pbr/Makefile
 rm -r source.openwrt.melmac.net
